92896f4d4c0e0a5e39d2c8e7fc755fc7a53919d6
Sylvia <sylviaatef151@gmail.com>
2022-05-21 18:25:31 -0400

    Added my_strcpy function

--- c.codes/string.c
+++ c.codes/string.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+char *my_strcpy(char *t, char *s)
+{
+char *p = t;
+while (*t++ = *s++);
+return p;
+}
 int my_strlen(char *s)
 {
 char *p = s;
@@ -9,11 +15,13 @@ return (p - s);
 int main(void)
 {
 int i;
+char p1[32];
 char *s[] = {
 "Git tutorials",
 "Tutorials Points"
 };
 for (i = 0; i < 2; ++i)
 printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
 return 0;
 }
