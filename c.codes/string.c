3846f3e325d79918806695207b5d191dab90bd9b
Sylvia <sylviaatef151@gmail.com>
2022-05-21 18:18:07 -0400

    changed in the strings

--- c.codes/string.c
+++ c.codes/string.c
@@ -11,7 +11,7 @@ int main(void)
 int i;
 char *s[] = {
 "Git tutorials",
-"Tutorials Point"
+"Tutorials Points"
 };
 for (i = 0; i < 2; ++i)
 printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
